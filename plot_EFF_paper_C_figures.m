9f7f9723453cc7df98919c9d585128e521e876cb
JonGroJG <jonathan.gropp@weizmann.ac.il>
2020-09-18 18:00:14 +0300

    update

--- plot_EFF_paper_C_figures.m
+++ plot_EFF_paper_C_figures.m
@@ -1,15 +1,16 @@
-function plot_EFF_paper_C_figures(fig,sav,close_val)
 % 5.2.20
 % Methanogenesis EFF paper
 % Plot beta values for carbon and hydrogen isotopes
 
-local_path   = '/Users/jonathag/Dropbox (Weizmann Institute)/Jonathan_with_Itay/Paper 2 EFFs/';
+% Figures to plot
+fig(1) = 1;
+
 file_name    = 'alpha-beta.xlsx';
-betaCdat     = readtable([local_path,file_name],'Sheet',1);
-gammaCHdat   = readtable([local_path,file_name],'Sheet',6);
-gammaHHdat   = readtable([local_path,file_name],'Sheet',8);
-clumpedCHdat = readtable([local_path,file_name],'Sheet',5);
-clumpedHHdat = readtable([local_path,file_name],'Sheet',7);
+betaCdat     = readtable(file_name,'Sheet',1);
+gammaCHdat   = readtable(file_name,'Sheet',6);
+gammaHHdat   = readtable(file_name,'Sheet',8);
+clumpedCHdat = readtable(file_name,'Sheet',5);
+clumpedHHdat = readtable(file_name,'Sheet',7);
 
 plot_EFF_Cfig(betaCdat,gammaCHdat,clumpedCHdat,...
     gammaHHdat,clumpedHHdat,fig,sav)
@@ -71,18 +72,15 @@ betaLoc = betaC(:,1);
 % Horita 2001 - CO2-CH4
 Horita01Tc   = [200.6 251.2 301.4 348.5 409 449 496.5 550 598.5];
 Horita01klna = [34.24 29.33 25.19 21.98 19.26 16.93 15.29 13.87 12.7];
-Horita01std  = [0.12 0.19 0.11 0.21 0.11 0.1 0.14 0.19 0.15];
 
 % Kueter 2019 - CO2-CH4
 Kueter19Tc   = [300 400 600 800 1000 1200];
 Kueter19klna = [25.2 20.1 11.7 8.3 6.9 4.7];
-Kueter19std  = [0.5 0.6 0.7 0.2 0.2 0.2];
 
 %% Theoretical data from the literture
 Bottinga69klna = 2.28e6./(Tk.^2) + 15.176e3./Tk - 8.38; % CO2-CH4, 0 - 700 oc
 Richet77klna   = -0.62e9./(Tk.^3) + 6.616e6./(Tk.^2) + 6.04e3./Tk - 3.08; % CO2-CH4, 400 - 1300 oc
-Mook74klna1    = 9.866e3./Tk - 24.12; % [HCO3-] - CO2(aq) 25-125 oc
-Mook74klna2    = 9.552e3./Tk - 24.10; % [HCO3-] - CO2(aq) 25-125 oc
+
 %%
 col(1,:)  = [0.655 0.808 0.890]; % Fmd / CO2
 col(2,:)  = [0.122 0.471 0.706]; % Ftr / CHO-MFR
@@ -96,10 +94,8 @@ col(9,:)  = [0.792 0.698 0.839]; % Acs / CH3-OH
 col(10,:) = [0.416 0.239 0.604]; % Cdh / CH3-COOH
 col(11,:) = [0.694 0.349 0.157]; % 
 
-%% FIGURE 1 - 13BETA VALUES FOR 1-100 oc
+%% 13BETA VALUES FOR 1-100 oc
 if fig(1) == 1
-    textLab1 = {'CO$_2$(g) [+4]','CHO-MFR [+2]','CHO-H$_4$MPT [+2]','CH-H$_4$MPT [+2]','CH$_2$-H$_4$MPT [0]',...
-        'CH$_3$-H$_4$MPT [-2]','CH$_3$-S-CoM [-2]','CH$_3$-OH [-2]','CH$_3$-COOH [-3]','CH$_4$(g) [-4]'};
     textLab1 = {'CO_2(g) [+4]','CHO-MFR [+2]','CHO-H_4MPT [+2]','CH-H_4MPT [+2]','CH_2-H_4MPT [0]',...
         'CH_3-H_4MPT [-2]','CH_3-SCoM [-2]','CH_3-OH [-2]','CH_3-COOH [-3]','CH_4(g) [-4]'};
     
@@ -144,44 +140,22 @@ if fig(1) == 1
         print('-f1',sprintf('%s13beta',pathstr),'-depsc');
     end
 end
-%% FIGURE 2A - 13ALPHA VALUES
+%% 13ALPHA VALUES FOR ENZYMES
 if fig(2) == 1
     textLab2 = {'Fmd';'Ftr';'Mch';'Mtd';'Mer';'Mtr';'Mcr';'Mta';'Acs';'Cdh'};
     alphaLoc = alphaC(:,51);
     x1 = betCdat.temp_C;
     
-    offset = 0.4;
     figure(2)
     set(2,'Units','centimeters','Position',[10 5 figWidth figHeight])
     for ja = 1:10
         ax1(ja) = plot(x1(1:51),1000.*log(alphaC((ja),1:51)),'Color',col(ja,:),...
             'LineWidth',figLineWidth);
-%         if ja == 4
-%             t = text(103,1000.*log(alphaLoc(ja,1))+offset,textLab2{ja});
-%         elseif ja == 6
-%             t = text(103,1000.*log(alphaLoc(ja,1))-offset,textLab2{ja});
-%         elseif ja == 10
-%             t = text(103,1000.*log(alphaLoc(ja,1))+offset,textLab2{ja});
-%         elseif ja == 3
-%             t = text(103,1000.*log(alphaLoc(ja,1))-offset,textLab2{ja});
-%         elseif ja == 1
-%             t = text(103,1000.*log(alphaLoc(ja,1))-0.4,textLab2{ja});
-%         elseif ja == 9
-%             t = text(103,1000.*log(alphaLoc(ja,1))+0.4,textLab2{ja});
-%         else
             t = text(103,1000.*log(alphaLoc(ja,1)),textLab2{ja});
-%         end
         t.FontName = 'Helvetica';
         t.FontSize = 16;
         hold on
     end
-%     hl(1).leg = legendflex(ax1, textLab2, 'anchor', [3 3], ...
-%         'buffer', [-3 -3], ...
-%         'ncol', 2, ...
-%         'fontsize', 14, ...
-%         'xscale', 1, ...
-%         'box', 'on',...
-%         'FontName','Helvetica');
     box off
     xlabel('T (\circC)')
     ylabel(['1000ln^{13}\alpha^{eq} (' char(8240) ')'])
@@ -196,7 +170,7 @@ if fig(2) == 1
     end
 end
 
-%% FIGURE 2B - 13ALPHA VALUES
+%% 13ALPHA VALUES RELATIVE TO CO2
 if fig(3) == 1
     textLab2 = {'CHO-MFR [+2]','CHO-H_4MPT [+2]','CH-H_4MPT [+2]',...
         'CH_2-H_4MPT [0]','CH_3-H_4MPT [-2]','CH_3-SCoM [-2]',...
@@ -234,7 +208,7 @@ if fig(3) == 1
     end
 end
 
-%% FIGURE 3 - CO2-CH4 COMPARISON
+%% COMPARISON OF 13ALPHA VALUES FOR CO2-CH4
 if fig(4) == 1
     figure(4)
     set(4,'Units','centimeters','Position',[10 5 figWidth figHeight])
@@ -244,26 +218,21 @@ if fig(4) == 1
     h3 = plot(x_axis,1000.*log(betaC(1,:)./betaC(2,:)),'k','LineWidth',figLineWidth);
     hold on
 
-%     h1 = plot(x_axis,Bottinga69klna,'LineWidth',figLineWidth);
-%     h1.Color = [0.651 0.808 0.890];%[1 1 1].*0.6;
-%     h1.LineStyle = '--';
-%     hold on
-
     h2 = plot(x_axis,Richet77klna,'k','LineWidth',figLineWidth);
-    h2.Color = [31 120 180]./255;%[0.122 0.471 0.706];
+    h2.Color = [31 120 180]./255;
     h2.LineStyle = ':';
     hold on
     
     h4 = plot(1000./(Horita01Tc+273.15),Horita01klna,'o');
     h4.MarkerEdgeColor = 'k';
-    h4.MarkerFaceColor = 'w';%[0.200 0.627 0.173];%[1 1 1].*0.9;
+    h4.MarkerFaceColor = 'w';
     h4.MarkerSize = 10;
     h4.LineWidth = 0.5;
     hold on
     
     h5 = plot(1000./(Kueter19Tc(1:3)+273.15),Kueter19klna(1:3),'o');
     h5.MarkerEdgeColor = 'k';
-    h5.MarkerFaceColor = [1 1 1]-0.5;%[0.698 0.875 0.541];%[1 1 1].*0.6;
+    h5.MarkerFaceColor = [1 1 1]-0.5;
     h5.MarkerSize = 10;
     h5.LineWidth = 0.5;
     hold on
@@ -276,9 +245,7 @@ if fig(4) == 1
     l.Location = 'northwest';
     l.Box = 'on';
     l.FontSize = 14;
-    % l.EdgeColor = 'w';
     grid on
-%     grid minor
     xlabel('1000/T (K)')
     ylabel(['1000ln^{13}\alpha_{CO_2(g)-CH_4(g)} (' char(8240) ')'])
     set(gca,'FontSize',figFontSize')
@@ -290,7 +257,7 @@ if fig(4) == 1
     end
 end
 
-%% FIGURE 5 - Clumped 13C-D isotopologues
+%% CLUMPED ISOTOPOLOGUES (13--D)
 if fig(5) == 1
     figure(5)
     set(5,'Units','centimeters','Position',[10 5 figWidth figHeight])
@@ -308,13 +275,6 @@ if fig(5) == 1
         t.FontName = 'Helvetica';
         t.FontSize = 14;
     end
-%     hl(1).leg = legendflex(ax, Deltalab, 'anchor', [3 3], ...
-%         'buffer', [-3 -3], ...
-%         'ncol', 2, ...
-%         'fontsize', 14, ...
-%         'xscale', 1, ...
-%         'box', 'on',...
-%         'FontName','Helvetica');
     xlim([0 150])
     ylim([2.7 6.7])
     grid on
@@ -329,61 +289,7 @@ if fig(5) == 1
     end
 end
 
-%% FIGURE 6 - Clumped 13C-D isotopologues (gamma values)
-if fig(6) == 1    
-    figure(6)
-    set(6,'Units','centimeters','Position',[10 5 figWidth figHeight])
-    alphalab_leg = {'Fmd','Ftr','Mch','Mtd','Mer','Mtr','Mcr','Mta','Acs','Cdh'}; 
-  
-    pos_s = [2:5,7:8,13:15];
-    pos_p = [1,9,11];
-    pos_s_col = [2:6,8:10];
-    pos_p_col = [1,4,5];
-    
-    maxval = 55;
-    x = gammaCHdat.T_C(1:4:maxval);
-    hold all
-    ax2(1)  = plot(x,10.*gammaCHdat.Fmd_p(1:4:maxval),'Color',col(1,:),'LineWidth',figLineWidth);
-    ax2(2)  = plot(x,gammaCHdat.Ftr_s(1:4:maxval),    'Color',col(2,:),'LineWidth',figLineWidth);
-    ax2(3)  = plot(x,gammaCHdat.Mch_s(1:4:maxval),    'Color',col(3,:),'LineWidth',figLineWidth);
-    ax2(4)  = plot(x,gammaCHdat.Mtd_s(1:4:maxval),    'Color',col(4,:),'LineWidth',figLineWidth);
-    ax2(5)  = plot(x,gammaCHdat.Mer_R_s(1:4:maxval),  'Color',col(5,:),'LineWidth',figLineWidth);
-    ax2(6)  = plot(x,gammaCHdat.Mtr_s(1:4:maxval),    'Color',col(6,:),'LineWidth',figLineWidth);
-    ax2(7)  = plot(x,gammaCHdat.Mcr_s(1:4:maxval),    'Color',col(7,:),'LineWidth',figLineWidth);
-    ax2(8)  = plot(x,gammaCHdat.Mta_s(1:4:maxval),    'Color',col(8,:),'LineWidth',figLineWidth);
-    ax2(9)  = plot(x,gammaCHdat.Acs_s(1:4:maxval),    'Color',col(9,:),'LineWidth',figLineWidth);
-    ax2(10) = plot(x,gammaCHdat.Cdh_s(1:4:maxval),    'Color',col(10,:),'LineWidth',figLineWidth);
-    ax2(11) = plot(x,gammaCHdat.Fmd_p(1:4:maxval),    'Color',col(1,:),'LineWidth',figLineWidth);
-    ax2(12) = plot(x,gammaCHdat.Mtd_p(1:4:maxval),    'Color',col(4,:),'LineWidth',figLineWidth);
-    ax2(13) = plot(x,gammaCHdat.Mer_p(1:4:maxval),    'Color',col(5,:),'LineWidth',figLineWidth);
-    ax2(14) = plot(x,gammaCHdat.Mcr_p(1:4:maxval),    'Color',col(7,:),'LineWidth',figLineWidth);
-    ax2(15) = plot(x,gammaCHdat.Hmd_p(1:4:maxval),    'Color',col(2,:),'LineWidth',figLineWidth);
-%     ax2(16) = plot(x,gammaCHdat.Mer_S_s(1:4:maxval),  'Color',col(5,:),'LineWidth',figLineWidth);
-    
-    for i = 11:15; ax2(i).LineStyle = '-.'; end
-%     ax2(16).LineStyle = ':'; 
-
-    legendflex(ax2(1:10), alphalab_leg, 'anchor', [5 5], ...
-        'buffer', [-10 10], ...
-        'ncol', 2, ...
-        'fontsize', 14, ...
-        'xscale', 0.7, ...
-        'box', 'on',...
-        'FontName','Helvetica');
-    xlim([0 100])
-    ylim([0.993 1.0006])
-    grid on
-    grid minor
-    xlabel('T (\circC)')
-    ylabel('\gamma')
-    set(gca,'FontSize',figFontSize)
-    if sav == 1
-        print('-f6',sprintf('%sgamma_132',pathstr),'-dpng','-r600');
-        print('-f6',sprintf('%sgamma_132',pathstr),'-depsc');
-    end
-end
-
-%% FIGURE 7 - Clumped D-D isotopologues 
+%% CLUMPED ISOTOPOLOGUES (D--D)
 if fig(7) == 1
     figure(7)
     set(7,'Units','centimeters','Position',[10 5 figWidth figHeight])
@@ -413,7 +319,7 @@ if fig(7) == 1
     end
 end
 
-%% FIGURE 4 - 12CH2D2 comparison
+%% COMPARISON OF DELTA(12CH2D2) OF METHANE
 if fig(8) == 1
     figure(8)
     set(8,'Units','centimeters','Position',[10 5 figWidth figHeight])
@@ -462,7 +368,7 @@ if fig(8) == 1
     end
 end
 
-%% FIGURE 4 - 13CH3D comparison
+%% COMPARISON OF DELTA(12CH3D) OF METHANE
 if fig(9) == 1
     figure(9)
     set(9,'Units','centimeters','Position',[10 5 figWidth figHeight])
@@ -508,16 +414,12 @@ if fig(9) == 1
         'Location','northeast','Box','off');
     xlabel('T (\circC)')
     ylabel(['\Delta_{^{13}CH_3D}^{eq} (' char(8240) ')'])
-%     ylim([-1.5 25])
     xlim([0 700])
     set(gca,'FontSize',figFontSize)
     box off
     if sav == 1
-%         print('-f8',sprintf('%sDelta_12CH2D2',pathstr),'-dpng','-r600');
         print('-f9',sprintf('%sDelta_13CH3D',pathstr),'-depsc');
     end
-end
-
 end
 
 end
